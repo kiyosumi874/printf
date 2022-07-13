3dc28a66b7c69dba45b32322da71422483d5de43
kiyosumi874 <shiiharakiyosumi@gmail.com>
2022-07-13 12:03:44 +0900

    InputClassの2P対応

--- GameOverScene.cpp
+++ GameOverScene.cpp
@@ -12,11 +12,11 @@ GameOverScene::~GameOverScene()
 
 TAG_SCENE GameOverScene::Update()
 {
-	if (Input::IsDown(BUTTON_ID_START))
+	if (Input::IsDown1P(BUTTON_ID_START))
 	{
 		return TAG_SCENE::TAG_TITLE;
 	}
-	if (Input::IsDown(BUTTON_ID_BACK))
+	if (Input::IsDown1P(BUTTON_ID_BACK))
 	{
 		return TAG_SCENE::TAG_END;
 	}
--- Input.h
+++ Input.h
@@ -20,36 +20,39 @@ enum ButtonID
 
 enum PAD_ID
 {
-    PAD_ID_DOWN =  (0x0001) ,// ↓チェックマスク
-    PAD_ID_LEFT =  (0x0002)	,// ←チェックマスク
-    PAD_ID_RIGHT = (0x0004)	,// →チェックマスク
-    PAD_ID_UP =    (0x0008)	,// ↑チェックマスク
-    PAD_ID_A =     (0x0010)	,// Ａボタンチェックマスク
-    PAD_ID_B =     (0x0020)	,// Ｂボタンチェックマスク
-    PAD_ID_X =     (0x0040)	,// Ｘボタンチェックマスク
-    PAD_ID_Y =     (0x0080)	,// Ｙボタンチェックマスク
-    PAD_ID_L =     (0x0100)	,// Ｌボタンチェックマスク
-    PAD_ID_R =     (0x0200)	,// Ｒボタンチェックマスク
-    PAD_ID_BACK =  (0x0400)	,// BACK
+    PAD_ID_DOWN = (0x0001),// ↓チェックマスク
+    PAD_ID_LEFT = (0x0002),// ←チェックマスク
+    PAD_ID_RIGHT = (0x0004),// →チェックマスク
+    PAD_ID_UP = (0x0008),// ↑チェックマスク
+    PAD_ID_A = (0x0010),// Ａボタンチェックマスク
+    PAD_ID_B = (0x0020),// Ｂボタンチェックマスク
+    PAD_ID_X = (0x0040),// Ｘボタンチェックマスク
+    PAD_ID_Y = (0x0080),// Ｙボタンチェックマスク
+    PAD_ID_L = (0x0100),// Ｌボタンチェックマスク
+    PAD_ID_R = (0x0200),// Ｒボタンチェックマスク
+    PAD_ID_BACK = (0x0400),// BACK
     PAD_ID_START = (0x0800)	 // ＳＴＡＲＴボタンチェックマスク
 
 };
 const int KEYBOARD = 0;
-const int CONTROLLER = 1;
+const int CONTROLLER_1P = 1;
+const int CONTROLLER_2P = 2;
+
 class Input
 {
 private:
     static Input* m_input;
     struct KeyInformation
     {
-        int keyCode[2]; // VK_SHIFTとか'A'のやつh
-        int pressCount[2]; // 状態
+        int keyCode[3]; // VK_SHIFTとか'A'のやつh
+        int pressCount[3]; // 状態
     };
     KeyInformation m_keys[BUTTON_ID_MAX];
+
     int m_joyPadNum;
     Input();
     ~Input();
-    
+
 
 public:
     Input(const Input&) = delete; // コピーコンストラクタを delete 指定。
@@ -72,18 +75,26 @@ public:
             delete m_input;
             m_input = nullptr;
         }
-       
+
     }
 
     static void Update();
-    // 押した瞬間
-    static bool IsDown(int buttonID) { return ((m_input->m_keys[buttonID].pressCount[KEYBOARD] == 1) || (m_input->m_keys[buttonID].pressCount[CONTROLLER] == 1)); }
-    // 押しているとき
-    static bool IsPress(int buttonID) { return m_input->m_keys[buttonID].pressCount[KEYBOARD] > 1 || m_input->m_keys[buttonID].pressCount[CONTROLLER] > 1; }
-    // 離した瞬間
-    static bool IsUp(int buttonID) { return m_input->m_keys[buttonID].pressCount[KEYBOARD] == 0 || m_input->m_keys[buttonID].pressCount[CONTROLLER] == 0; }
-    // 離しているとき
-    static bool IsRelease(int buttonID) { return m_input->m_keys[buttonID].pressCount[KEYBOARD] < 0 || m_input->m_keys[buttonID].pressCount[CONTROLLER] < 0; }
+    // 1Pが押した瞬間
+    static bool IsDown1P(int buttonID) { return ((m_input->m_keys[buttonID].pressCount[KEYBOARD] == 1) || (m_input->m_keys[buttonID].pressCount[CONTROLLER_1P] == 1)); }
+    // 1Pが押しているとき
+    static bool IsPress1P(int buttonID) { return m_input->m_keys[buttonID].pressCount[KEYBOARD] > 1 || m_input->m_keys[buttonID].pressCount[CONTROLLER_1P] > 1; }
+    // 1Pが離した瞬間
+    static bool IsUp1P(int buttonID) { return m_input->m_keys[buttonID].pressCount[KEYBOARD] == 0 || m_input->m_keys[buttonID].pressCount[CONTROLLER_1P] == 0; }
+    // 1Pが離しているとき
+    static bool IsRelease1P(int buttonID) { return m_input->m_keys[buttonID].pressCount[KEYBOARD] < 0 || m_input->m_keys[buttonID].pressCount[CONTROLLER_1P] < 0; }
 
 
+    // 2Pが押した瞬間
+    static bool IsDown2P(int buttonID) { return m_input->m_keys[buttonID].pressCount[CONTROLLER_2P] == 1; }
+    // 2Pが押しているとき
+    static bool IsPress2P(int buttonID) { return m_input->m_keys[buttonID].pressCount[CONTROLLER_2P] > 1; }
+    // 2Pが離した瞬間
+    static bool IsUp2P(int buttonID) { return m_input->m_keys[buttonID].pressCount[CONTROLLER_2P] == 0; }
+    // 2Pが離しているとき
+    static bool IsRelease2P(int buttonID) { return m_input->m_keys[buttonID].pressCount[CONTROLLER_2P] < 0; }
 };
